ed0014790953018ac9372048f8863be824f46808
GigiMZ <gigitsiklaurimz@gmail.com>
2025-05-13 23:43:50 +0400

    fixing post/serializer

--- post/serializer.py
+++ post/serializer.py
@@ -1,20 +1,28 @@
 from rest_framework import serializers
 from .models import Comment, Post, Tag
 
-
+# TODO fix serializer
 class PostSerializer(serializers.ModelSerializer):
+    # author = serializers.CharField(source='author.username')
+    comment = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['author', 'title', 'content', 'image', 'tags', 'comment', 'vote_up', 'vote_down', 'views']
-        read_only_fields  = ['date']
+        fields = ['id', 'author', 'title', 'content', 'image', 'tags', 'comment', 'vote_up', 'vote_down', 'views']
+        read_only_fields  = ['date', 'author']
 
+    def get_comment(self, obj):
+        return CommentSerializer(obj.comment).data
 
 class CommentSerializer(serializers.ModelSerializer):
+    # author = serializers.CharField(source='author.username', read_only=True)
+    comment = serializers.SerializerMethodField()
     class Meta:
         model = Comment
-        fields = ['author', 'content', 'parent', 'vote_up', 'vote_down', 'views']
+        fields = ['id', 'author', 'content', 'comment', 'vote_up', 'vote_down']
         read_only_fields = ['date']
 
+    def get_comment(self, obj):
+        return CommentSerializer(obj.comment).data
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:

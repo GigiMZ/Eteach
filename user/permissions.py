0eaf07f4662e6e9a9511677672ab7018ea8323e6
GigiMZ <gigitsiklaurimz@gmail.com>
2025-05-13 22:28:47 +0400

    added description for premissions

--- user/permissions.py
+++ user/permissions.py
@@ -1,17 +1,14 @@
 from rest_framework import permissions
 
 
+# Clients can only edit their own profile
 class UserEditPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.method in ['PUT', 'PATCH', 'GET'] and obj.username == request.user.username
 
 
+# Clients can only create account while being logged out
 class UserCreatePermission(permissions.BasePermission):
     message = "You are logged in."
     def has_permission(self, request, view):
         return not (request.method == 'POST' and request.user.is_authenticated)
-
-
-class PostCreateEditPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or request.user.is_authenticated

b402f09f7763b9e9ba3451b2891a4eb6cc05a8a2
GigiMZ <gigitsiklaurimz@gmail.com>
2025-05-25 22:22:09 +0400

    added vote_up and vote_down features, user autoselect while createing post

--- post/urls.py
+++ post/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.PostListCreateAPIView.as_view()),
-    path('posts/<int:pk>', views.PostRetrieveUpdateDestroyAPIView.as_view()),
+    path('posts/<int:pk>/', views.PostRetrieveUpdateDestroyAPIView.as_view()),
+    path('posts/<int:pk>/vote_up', views.post_up_vote),
+    path('posts/<int:pk>/vote_down', views.post_down_vote),
     path('comments/', views.CommentListCreateAPIView.as_view()),
-    path('comments/<int:pk>', views.CommentRetrieveUpdateDestroyAPIView.as_view()),
+    path('comments/<int:pk>/', views.CommentRetrieveUpdateDestroyAPIView.as_view()),
     path('tags/', views.TagListCreateAPIView.as_view()),
     path('tags/<int:pk>', views.TagRetrieveUpdateDestroyAPIView.as_view())
 ]
